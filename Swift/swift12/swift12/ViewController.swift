6e513ae088342fcdd9c64cd94d65267b6d23214f
Arron Yang <yangzhwork@126.com>
2016-07-14 19:11:47 +0800

    swift12 spring动画

---
+++ Swift/swift12/swift12/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  swift12
+//
+//  Created by Aaron on 16/7/14.
+//  Copyright © 2016年 Aaron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var firstTF: UITextField!
+    @IBOutlet weak var secTF: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func loginBtnClicked(sender: AnyObject) {
+
+        
+    }
+
+}
+
