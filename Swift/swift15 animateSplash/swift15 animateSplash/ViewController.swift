1e2ca0da46fdb760abab96bc9f2698456093eae3
Arron Yang <yangzhwork@126.com>
2016-07-18 09:12:32 +0800

    swift15

---
+++ Swift/swift15 animateSplash/swift15 animateSplash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift15 animateSplash
+//
+//  Created by Aaron on 16/7/15.
+//  Copyright © 2016年 Aaron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
